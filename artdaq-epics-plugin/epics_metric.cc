9aa024b7e5f33b78ed3108ffe001d0944361a6ea
Eric Flumerfelt <eflumerf@fnal.gov>
2017-03-24 13:45:13 -0500

    Applied code style in Visual Studio.

--- artdaq-epics-plugin/epics_metric.cc
+++ artdaq-epics-plugin/epics_metric.cc
@@ -13,134 +13,140 @@
 #undef STATIC_ASSERT
 #include "cadef.h"
 
-namespace artdaq {
-class EpicsMetric : public MetricPlugin
+namespace artdaq
 {
-private:
-	std::string prefix_;
-	std::unordered_map<std::string, chid> channels_;
-
-	bool checkChannel_(std::string name)
+	class EpicsMetric : public MetricPlugin
 	{
-		if (!channels_.count(name))
+	private:
+		std::string prefix_;
+		std::unordered_map<std::string, chid> channels_;
+
+		bool checkChannel_(std::string name)
 		{
-			chid channel;
-			int sts = ca_search(name.c_str(), &channel);
-			sts = ca_pend_io(5.0);
-			if (sts != ECA_NORMAL)
+			if (!channels_.count(name))
 			{
-				SEVCHK(ca_clear_channel(channel), NULL);
-				mf::LogWarning("EPICS Plugin") << "Channel " << name << " not found!";
-				channels_[name] = nullptr;
-				return false;
+				chid channel;
+				int sts = ca_search(name.c_str(), &channel);
+				sts = ca_pend_io(5.0);
+				if (sts != ECA_NORMAL)
+				{
+					SEVCHK(ca_clear_channel(channel), NULL);
+					mf::LogWarning("EPICS Plugin") << "Channel " << name << " not found!";
+					channels_[name] = nullptr;
+					return false;
+				}
+				channels_[name] = channel;
+				return true;
 			}
-			channels_[name] = channel;
-			return true;
-		}
-		return channels_[name] != nullptr;
-	}
-
-public:
-	EpicsMetric(fhicl::ParameterSet pset)
-		: MetricPlugin(pset)
-		, prefix_(pset.get<std::string>("channel_name_prefix", "artdaq"))
-		, channels_()
-	{}
-
-	virtual ~EpicsMetric()
-	{
-		stopMetrics();
-	}
-
-	std::string getLibName() const { return "epics"; }
-	void stopMetrics_()
-	{
-		for (auto channel : channels_)
-		{
-			SEVCHK(ca_clear_channel(channel.second), NULL);
-		}
-		channels_.clear();
-	}
-	void startMetrics_() {}
-
-	void sendMetric_(const std::string& name, const std::string& value, const std::string& unit)
-	{
-		std::string caName = prefix_ + ":" + name;
-		std::string tmpValue = value + " " + unit;
-
-		if (checkChannel_(caName))
-		{
-			//DBR_STRING, 40 characters
-			if (tmpValue.size() > 40) { tmpValue = tmpValue.erase(40); }
-			SEVCHK(ca_put(DBR_STRING, channels_[caName], tmpValue.c_str()), NULL);
-			SEVCHK(ca_flush_io(), NULL);
-		}
-	}
-	void sendMetric_(const std::string& name,const int& value, const std::string& unit)
-	{
-		//DBR_LONG
-		std::string caName = prefix_ + ":" + name;
-		if (unit.size() > 0)
-		{
-			mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			return channels_[name] != nullptr;
 		}
 
-		if (checkChannel_(caName))
+	public:
+		EpicsMetric(fhicl::ParameterSet pset)
+			: MetricPlugin(pset)
+			, prefix_(pset.get<std::string>("channel_name_prefix", "artdaq"))
+			, channels_() {}
+
+		virtual ~EpicsMetric()
 		{
-			dbr_long_t val = static_cast<dbr_long_t>(value);
-			SEVCHK(ca_put(DBR_LONG, channels_[caName], &val), NULL);
-			SEVCHK(ca_flush_io(), NULL);
-		}
-	}
-	void sendMetric_(const std::string& name,const double& value, const std::string& unit)
-	{
-		//DBR_DOUBLE
-		std::string caName = prefix_ + ":" + name;
-		if (unit.size() > 0)
-		{
-			mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			stopMetrics();
 		}
 
-		if (checkChannel_(caName))
+		std::string getLibName() const { return "epics"; }
+
+		void stopMetrics_()
 		{
-			dbr_double_t val = static_cast<dbr_double_t>(value);
-			SEVCHK(ca_put(DBR_DOUBLE, channels_[caName], &val), NULL);
-			SEVCHK(ca_flush_io(), NULL);
-		}
-	}
-	void sendMetric_(const std::string& name, const float& value, const std::string& unit)
-	{
-		//DBR_FLOAT
-		std::string caName = prefix_ + ":" + name;
-		if (unit.size() > 0)
-		{
-			mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			for (auto channel : channels_)
+			{
+				SEVCHK(ca_clear_channel(channel.second), NULL);
+			}
+			channels_.clear();
 		}
 
-		if (checkChannel_(caName))
+		void startMetrics_() {}
+
+		void sendMetric_(const std::string& name, const std::string& value, const std::string& unit)
 		{
-			dbr_float_t val = static_cast<dbr_float_t>(value);
-			SEVCHK(ca_put(DBR_FLOAT, channels_[caName], &val), NULL);
-			SEVCHK(ca_flush_io(), NULL);
-		}
-	}
-	void sendMetric_(const std::string& name, const unsigned long int& value, const std::string& unit)
-	{
-		//DBR_LONG, only unsigned type is only 16 bits, use widest integral field
-		std::string caName = prefix_ + ":" + name;
-		if (unit.size() > 0)
-		{
-			mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			std::string caName = prefix_ + ":" + name;
+			std::string tmpValue = value + " " + unit;
+
+			if (checkChannel_(caName))
+			{
+				//DBR_STRING, 40 characters
+				if (tmpValue.size() > 40) { tmpValue = tmpValue.erase(40); }
+				SEVCHK(ca_put(DBR_STRING, channels_[caName], tmpValue.c_str()), NULL);
+				SEVCHK(ca_flush_io(), NULL);
+			}
 		}
 
-		if (checkChannel_(caName))
+		void sendMetric_(const std::string& name, const int& value, const std::string& unit)
 		{
-			dbr_long_t val = static_cast<dbr_long_t>(value);
-			SEVCHK(ca_put(DBR_LONG, channels_[caName], &val), NULL);
-			SEVCHK(ca_flush_io(), NULL);
+			//DBR_LONG
+			std::string caName = prefix_ + ":" + name;
+			if (unit.size() > 0)
+			{
+				mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			}
+
+			if (checkChannel_(caName))
+			{
+				dbr_long_t val = static_cast<dbr_long_t>(value);
+				SEVCHK(ca_put(DBR_LONG, channels_[caName], &val), NULL);
+				SEVCHK(ca_flush_io(), NULL);
+			}
 		}
-	}
-};
+
+		void sendMetric_(const std::string& name, const double& value, const std::string& unit)
+		{
+			//DBR_DOUBLE
+			std::string caName = prefix_ + ":" + name;
+			if (unit.size() > 0)
+			{
+				mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			}
+
+			if (checkChannel_(caName))
+			{
+				dbr_double_t val = static_cast<dbr_double_t>(value);
+				SEVCHK(ca_put(DBR_DOUBLE, channels_[caName], &val), NULL);
+				SEVCHK(ca_flush_io(), NULL);
+			}
+		}
+
+		void sendMetric_(const std::string& name, const float& value, const std::string& unit)
+		{
+			//DBR_FLOAT
+			std::string caName = prefix_ + ":" + name;
+			if (unit.size() > 0)
+			{
+				mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			}
+
+			if (checkChannel_(caName))
+			{
+				dbr_float_t val = static_cast<dbr_float_t>(value);
+				SEVCHK(ca_put(DBR_FLOAT, channels_[caName], &val), NULL);
+				SEVCHK(ca_flush_io(), NULL);
+			}
+		}
+
+		void sendMetric_(const std::string& name, const unsigned long int& value, const std::string& unit)
+		{
+			//DBR_LONG, only unsigned type is only 16 bits, use widest integral field
+			std::string caName = prefix_ + ":" + name;
+			if (unit.size() > 0)
+			{
+				mf::LogDebug("EPICS Plugin") << "Not sure if I can send ChannelAccess Units...configure in db instead.";
+			}
+
+			if (checkChannel_(caName))
+			{
+				dbr_long_t val = static_cast<dbr_long_t>(value);
+				SEVCHK(ca_put(DBR_LONG, channels_[caName], &val), NULL);
+				SEVCHK(ca_flush_io(), NULL);
+			}
+		}
+	};
 } //End namespace artdaq
 
 DEFINE_ARTDAQ_METRIC(artdaq::EpicsMetric)
