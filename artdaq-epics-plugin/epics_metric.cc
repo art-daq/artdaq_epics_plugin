acf356dfa8a64b9ae2d8ac3619c99738f84e6a29
Eric Flumerfelt <eflumerf@fnal.gov>
2017-04-10 12:35:16 -0500

    Document artdaq_epics_plugin

--- artdaq-epics-plugin/epics_metric.cc
+++ artdaq-epics-plugin/epics_metric.cc
@@ -13,8 +13,14 @@
 #undef STATIC_ASSERT
 #include "cadef.h"
 
+/**
+ * \brief The artdaq namespace
+ */
 namespace artdaq
 {
+	/**
+	 * \brief An instance of the MetricPlugin class that sends metric data using the Channel Access protocol from EPICS.
+	 */
 	class EpicsMetric : public MetricPlugin
 	{
 	private:
@@ -26,7 +32,7 @@ namespace artdaq
 			if (!channels_.count(name))
 			{
 				chid channel;
-				int sts = ca_search(name.c_str(), &channel);
+				auto sts = ca_search(name.c_str(), &channel);
 				sts = ca_pend_io(5.0);
 				if (sts != ECA_NORMAL)
 				{
@@ -42,19 +48,30 @@ namespace artdaq
 		}
 
 	public:
-		EpicsMetric(fhicl::ParameterSet pset)
+		/**
+		 * \brief Construct an instance of the EpicsMetric plugin.
+		 * \param pset Parameter set to configure with. MetricPlugin parameters plus "channel_name_prefix", default "artdaq".
+		 */
+		explicit EpicsMetric(fhicl::ParameterSet pset)
 			: MetricPlugin(pset)
 			, prefix_(pset.get<std::string>("channel_name_prefix", "artdaq"))
 			, channels_() {}
 
 		virtual ~EpicsMetric()
 		{
-			stopMetrics();
+			MetricPlugin::stopMetrics();
 		}
 
-		std::string getLibName() const { return "epics"; }
+		/**
+		 * \brief Gets the unique library name of this plugin
+		 * \return The library name of this plugin, "epics".
+		 */
+		std::string getLibName() const override { return "epics"; }
 
-		void stopMetrics_()
+		/**
+		 * \brief Clears the registered ChannelAccess channels.
+		 */
+		void stopMetrics_() override
 		{
 			for (auto channel : channels_)
 			{
@@ -63,9 +80,22 @@ namespace artdaq
 			channels_.clear();
 		}
 
-		void startMetrics_() {}
+		/**
+		 * \brief No initialization is needed to start sending metrics.
+		 */
+		void startMetrics_() override {}
 
-		void sendMetric_(const std::string& name, const std::string& value, const std::string& unit)
+		/**
+		 * \brief Send a string metric data point to ChannelAccess.
+		 * \param name Name of the metric
+		 * \param value Value of the metric
+		 * \param unit Units used (not really relevant for string metrics)
+		 * 
+		 * Send a string metric data point to ChannelAccess. The name will be channel_name_prefix:name.
+		 * If the named channel is not yet open, it will be opened. If the channel is not registered with an
+		 * IOC, then the metric data will not be sent and a warning message will be printed the first time.
+		 */
+		void sendMetric_(const std::string& name, const std::string& value, const std::string& unit) override
 		{
 			std::string caName = prefix_ + ":" + name;
 			std::string tmpValue = value + " " + unit;
@@ -79,7 +109,17 @@ namespace artdaq
 			}
 		}
 
-		void sendMetric_(const std::string& name, const int& value, const std::string& unit)
+		/**
+		* \brief Send an integer metric data point to ChannelAccess.
+		* \param name Name of the metric
+		* \param value Value of the metric
+		* \param unit Units used
+		*
+		* Send a string metric data point to ChannelAccess. The name will be channel_name_prefix:name.
+		* If the named channel is not yet open, it will be opened. If the channel is not registered with an
+		* IOC, then the metric data will not be sent and a warning message will be printed the first time.
+		*/
+		void sendMetric_(const std::string& name, const int& value, const std::string& unit) override
 		{
 			//DBR_LONG
 			std::string caName = prefix_ + ":" + name;
@@ -96,7 +136,17 @@ namespace artdaq
 			}
 		}
 
-		void sendMetric_(const std::string& name, const double& value, const std::string& unit)
+		/**
+		* \brief Send a double metric data point to ChannelAccess.
+		* \param name Name of the metric
+		* \param value Value of the metric
+		* \param unit Units used
+		*
+		* Send a string metric data point to ChannelAccess. The name will be channel_name_prefix:name.
+		* If the named channel is not yet open, it will be opened. If the channel is not registered with an
+		* IOC, then the metric data will not be sent and a warning message will be printed the first time.
+		*/
+		void sendMetric_(const std::string& name, const double& value, const std::string& unit) override
 		{
 			//DBR_DOUBLE
 			std::string caName = prefix_ + ":" + name;
@@ -113,7 +163,17 @@ namespace artdaq
 			}
 		}
 
-		void sendMetric_(const std::string& name, const float& value, const std::string& unit)
+		/**
+		* \brief Send a float metric data point to ChannelAccess.
+		* \param name Name of the metric
+		* \param value Value of the metric
+		* \param unit Units used
+		*
+		* Send a string metric data point to ChannelAccess. The name will be channel_name_prefix:name.
+		* If the named channel is not yet open, it will be opened. If the channel is not registered with an
+		* IOC, then the metric data will not be sent and a warning message will be printed the first time.
+		*/
+		void sendMetric_(const std::string& name, const float& value, const std::string& unit) override
 		{
 			//DBR_FLOAT
 			std::string caName = prefix_ + ":" + name;
@@ -130,7 +190,17 @@ namespace artdaq
 			}
 		}
 
-		void sendMetric_(const std::string& name, const unsigned long int& value, const std::string& unit)
+		/**
+		* \brief Send an unsigned integer metric data point to ChannelAccess.
+		* \param name Name of the metric
+		* \param value Value of the metric. Will be truncated t fit in the size of a dbr_ulong_t, a 32-bit unsigned integer.
+		* \param unit Units used
+		*
+		* Send a string metric data point to ChannelAccess. The name will be channel_name_prefix:name.
+		* If the named channel is not yet open, it will be opened. If the channel is not registered with an
+		* IOC, then the metric data will not be sent and a warning message will be printed the first time.
+		*/
+		void sendMetric_(const std::string& name, const unsigned long int& value, const std::string& unit) override
 		{
 			//DBR_LONG, only unsigned type is only 16 bits, use widest integral field
 			std::string caName = prefix_ + ":" + name;
@@ -141,7 +211,7 @@ namespace artdaq
 
 			if (checkChannel_(caName))
 			{
-				dbr_long_t val = static_cast<dbr_long_t>(value);
+				dbr_ulong_t val = static_cast<dbr_ulong_t>(value);
 				SEVCHK(ca_put(DBR_LONG, channels_[caName], &val), NULL);
 				SEVCHK(ca_flush_io(), NULL);
 			}
